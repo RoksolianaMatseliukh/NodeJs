1131c469d3fdb3a884e923efbbd8d85aa8f6a543
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2021-01-09 19:53:10 +0200

    visual correction in routes

--- hw4-main-project/routes/car/car.router.js
+++ hw4-main-project/routes/car/car.router.js
@@ -6,14 +6,24 @@ const { authMiddlewares, carMiddlewares, userMiddlewares } = require('../../midd
 const carRouter = Router();
 
 carRouter.get('/', carController.getCars);
-                                                            // to check if the user is logged in to add a car
-carRouter.post('/', carMiddlewares.checkIsCarOwnerIdValid, authMiddlewares.checkAccessToken,
-    carMiddlewares.checkIsCarValidToCreate, carController.createCar);
+
+carRouter.post('/',
+    carMiddlewares.checkIsCarOwnerIdValid,
+    // to check if the user is logged in to add a car
+    authMiddlewares.checkAccessToken,
+    carMiddlewares.checkIsCarValidToCreate,
+    carController.createCar);
 
 carRouter.use('/:carId', userMiddlewares.checkIsIdValid);
-carRouter.get('/:carId', carMiddlewares.checkCarByParams, carController.getCarById);
-carRouter.put('/:carId', authMiddlewares.checkAccessToken, carMiddlewares.checkIsCarValidToEdit,
+carRouter.get('/:carId',
+    carMiddlewares.checkCarByParams,
+    carController.getCarById);
+carRouter.put('/:carId',
+    authMiddlewares.checkAccessToken,
+    carMiddlewares.checkIsCarValidToEdit,
     carController.editCarById);
-carRouter.delete('/:carId', authMiddlewares.checkAccessToken, carController.deleteCarById);
+carRouter.delete('/:carId',
+    authMiddlewares.checkAccessToken,
+    carController.deleteCarById);
 
 module.exports = carRouter;
--- hw4-main-project/routes/user/user.router.js
+++ hw4-main-project/routes/user/user.router.js
@@ -5,14 +5,25 @@ const { authMiddlewares, userMiddlewares } = require('../../middlewares');
 
 const userRouter = Router();
 
-userRouter.get('/', userMiddlewares.checkUserByQueries, userController.getUsersWithCars);
-userRouter.post('/', userMiddlewares.checkIsUserValidToCreate, userMiddlewares.checkIfUserAlreadyExists,
+userRouter.get('/',
+    userMiddlewares.checkUserByQueries,
+    userController.getUsersWithCars);
+userRouter.post('/',
+    userMiddlewares.checkIsUserValidToCreate,
+    userMiddlewares.checkIfUserAlreadyExists,
     userController.createUser);
 
 userRouter.use('/:userId', userMiddlewares.checkIsIdValid);
-userRouter.get('/:userId', userMiddlewares.checkUserByParams, userController.getUserById);
-userRouter.put('/:userId', authMiddlewares.checkAccessToken, userMiddlewares.checkIsUserValidToEdit,
-    userMiddlewares.checkIfUserAlreadyExists, userController.editUserById);
-userRouter.delete('/:userId', authMiddlewares.checkAccessToken, userController.deleteUserById);
+userRouter.get('/:userId',
+    userMiddlewares.checkUserByParams,
+    userController.getUserById);
+userRouter.put('/:userId',
+    authMiddlewares.checkAccessToken,
+    userMiddlewares.checkIsUserValidToEdit,
+    userMiddlewares.checkIfUserAlreadyExists,
+    userController.editUserById);
+userRouter.delete('/:userId',
+    authMiddlewares.checkAccessToken,
+    userController.deleteUserById);
 
 module.exports = userRouter;
