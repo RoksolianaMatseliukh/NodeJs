fce6315ceb641dede31efb09eaebc02d5b1a58b6
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2021-01-09 20:36:32 +0200

    update

--- hw4-main-project/constants/data-base.enum.js
+++ hw4-main-project/constants/data-base.enum.js
@@ -2,10 +2,6 @@ module.exports = {
     DATABASE_NAME: process.env.DATABASE_NAME,
     LOCALHOST: 'localhost',
     MYSQL: 'mysql',
-    PASSWORD: process.env.DATABASE_PASSWORD,
-    USER: process.env.DATABASE_USER,
-
-    NOW: 'now',
 
     // reference key
     ID: 'id',
--- hw4-main-project/constants/date.enum.js
+++ hw4-main-project/constants/date.enum.js
@@ -1,4 +1,6 @@
 module.exports = {
     CURRENT_YEAR: new Date().getFullYear(),
-    MIN_MANUFACTURE_CAR_YEAR: new Date().getFullYear() - 300
+    MIN_MANUFACTURE_CAR_YEAR: new Date().getFullYear() - 300,
+
+    NOW: 'now'
 };
